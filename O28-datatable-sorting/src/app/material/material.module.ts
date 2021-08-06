a467dd517dd0b1d0ad54f476b10042c1b5dce5ae
inkushwahasonu <inkushwahasonu@gmail.com>
2021-08-06 13:51:36 +0530

    sorting and filterin of data in data table

--- O28-datatable-sorting/src/app/material/material.module.ts
+++ O28-datatable-sorting/src/app/material/material.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [],
-  imports: [MatTableModule, MatSortModule],
-  exports: [MatTableModule, MatSortModule],
+  imports: [
+    MatTableModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+  ],
+  exports: [
+    MatTableModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+  ],
 })
 export class MaterialModule {}
